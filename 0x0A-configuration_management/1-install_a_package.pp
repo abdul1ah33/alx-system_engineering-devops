a28ac03ba193fe5df3600cce9163367baeb4833a
abdul1ah33 <study.abdullah2005@gmail.com>
2024-07-09 00:43:53 -0700

    sdfj

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,12 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# 1-install_a_package.pp
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+  path    => ['/bin', '/usr/bin'],
+  require => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
 
